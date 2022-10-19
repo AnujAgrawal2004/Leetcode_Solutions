51924fd8fe95da7c6cf5d50c790270b9f860c0c9
AnujAgrawal2004 <90625022+AnujAgrawal2004@users.noreply.github.com>
2022-10-19 21:47:28 +0530

    Time: 5 ms (99.94%), Space: 39.9 MB (98.47%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.java
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.java
@@ -1,32 +1,27 @@
 class Solution {
     public int minDifficulty(int[] jobDifficulty, int d) {
-        long[][][] dp=new long[jobDifficulty.length+1][d+1][1001];
-        for(long[][] x:dp)
-         for(long[] y:x) Arrays.fill(y,-1);
-         int[] suffix=new int[jobDifficulty.length];
-         for(int i=jobDifficulty.length-1;i>=0;i--)
-         {
-             if(i==jobDifficulty.length-1)
-             suffix[i]=jobDifficulty[i];
-             else{
-                 suffix[i]=Math.max(suffix[i+1],jobDifficulty[i]);
-             }
-         }
-        long ans=helper(jobDifficulty,0,d,Integer.MIN_VALUE,dp,suffix);
-        return ans>=Integer.MAX_VALUE?-1:(int)ans;
-    }
-    public long helper(int[] jobDifficulty,int ind,int d,int max,long[][][]dp,int[] suffix)
-    {
-        if(ind>=jobDifficulty.length)return Integer.MAX_VALUE;
-        if((d-1)==0)
-        {
-            if(ind<jobDifficulty.length)return suffix[ind];
-            return Integer.MAX_VALUE;
+         int len=jobDifficulty.length;
+        if(d > len) return -1;
+        int [][]minDifficulty=new int[d][len];
+        for(int i=1;i<d;i++){
+            Arrays.fill(minDifficulty[i],Integer.MAX_VALUE);
         }
-        if(max!=Integer.MIN_VALUE && dp[ind][d][max]!=-1)return dp[ind][d][max];
-        max=Math.max(max,jobDifficulty[ind]);
-        long partition=max+helper(jobDifficulty,ind+1,d-1,Integer.MIN_VALUE,dp,suffix);
-        long ntpartition=helper(jobDifficulty,ind+1,d,max,dp,suffix);
-        return dp[ind][d][max]=Math.min(partition,ntpartition);
-    }
+        int maxDifficulty=0;
+        for(int i=0;i<=len-d;i++){
+            maxDifficulty=Math.max(maxDifficulty,jobDifficulty[i]);
+            minDifficulty[0][i]=maxDifficulty;
+        }
+        for(int day=1;day<d;day++){
+            for(int to=day;to<=len-d+day;to++){
+                int currentDayDifficulty=jobDifficulty[to];
+                int result=Integer.MAX_VALUE;
+                for(int j=to-1;j>=day-1;j--){
+                    result=Math.min(result,minDifficulty[day-1][j]+currentDayDifficulty);
+                    currentDayDifficulty=Math.max(currentDayDifficulty,jobDifficulty[j]);
+                }
+                minDifficulty[day][to]=result;
+            }
+        }
+        return  minDifficulty[d-1][len-1];
+        }
 }
