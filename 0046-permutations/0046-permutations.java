39c92a187dcb6c0f939713a1535446f1bff6a825
AnujAgrawal2004 <90625022+AnujAgrawal2004@users.noreply.github.com>
2023-04-04 09:51:40 +0530

    Time: 1 ms (95.22%), Space: 42.6 MB (59.45%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,22 +1,46 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans = new LinkedList<>();
-        backtrack(ans,new ArrayList<>(),nums);
-        return ans;
+       List<List<Integer>> res = new ArrayList<>();
+        if(nums.length<1) return res;
+        helper(res,new ArrayList<>(),nums,new boolean[nums.length]);
+        return res;
     }
-    public void backtrack(List<List<Integer>> ans,List<Integer> temp,int nums[]){
-      if(temp.size()==nums.length){
-          ans.add(new LinkedList<>(temp));
-      }  
-        else{
-            for(int i=0;i<nums.length;i++){
-                if(temp.contains(nums[i]))
-                    continue;
-                temp.add(nums[i]);
-                backtrack(ans,temp,nums);
-                temp.remove(temp.size()-1);
-            }
+    
+    private void helper(List<List<Integer>> res,List<Integer> cur,int nums[],boolean visited[]){
+        if(cur.size()==nums.length)
+        {
+            res.add(new ArrayList<>(cur));
+            return;
         }
         
+        for(int i=0;i<nums.length;i++){
+            if(visited[i]) continue;
+            cur.add(nums[i]);
+            visited[i]=true;
+            helper(res,cur,nums,visited);
+            cur.remove(cur.size()-1);
+            visited[i]=false;
+        }
     }
 }
+
+
+
+//  List<List<Integer>> ans = new LinkedList<>();
+//         backtrack(ans,new ArrayList<>(),nums);
+//         return ans;
+//     }
+//     public void backtrack(List<List<Integer>> ans,List<Integer> temp,int nums[]){
+//       if(temp.size()==nums.length){
+//           ans.add(new LinkedList<>(temp));
+//       }  
+//         else{
+//             for(int i=0;i<nums.length;i++){
+//                 if(temp.contains(nums[i]))
+//                     continue;
+//                 temp.add(nums[i]);
+//                 backtrack(ans,temp,nums);
+        
+//                 temp.remove(temp.size()-1);
+//             }
+//         }
