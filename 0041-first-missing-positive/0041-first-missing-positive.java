b8c9869631ffcb0fc29c37196516448d1303bd96
AnujAgrawal2004 <90625022+AnujAgrawal2004@users.noreply.github.com>
2023-04-10 09:46:04 +0530

    Time: 1 ms (99.96%), Space: 50.7 MB (76.44%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,28 +1,27 @@
 class Solution {
 public int firstMissingPositive(int[] nums) {
-    int n = nums.length;
-
-    // First pass: place each number in its correct position
-    for (int i = 0; i < n; i++) {
-        while (nums[i] > 0 && nums[i] <= n && nums[nums[i]-1] != nums[i]) {
-            int j = nums[i] - 1;
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
+    if(nums.length==0)
+        return nums.length + 1 ;
+    for(int i=0;i<nums.length;){
+        if(nums[i]>nums.length || nums[i]<=0)
+            i++;
+        else if(nums[nums[i]-1]==nums[i])
+            i++;
+        else{
+            int temp=nums[nums[i]-1];
+            nums[nums[i]-1]=nums[i];
+            nums[i]=temp;
         }
+            
     }
-
-    // Second pass: find the index of the first element that is not in its correct position
-    for (int i = 0; i < n; i++) {
-        if (nums[i] != i+1) {
+    int i=0;
+    for(i=0;i<nums.length;i++){
+        if(nums[i]!=i+1)
             return i+1;
-        }
     }
-
-    // If all elements are in their correct positions, the smallest missing positive integer is n+1
-    return n+1;
+    return nums[i-1]+1;
+    
 }
-
 }
 
 
