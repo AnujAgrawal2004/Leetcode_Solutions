2079bac2025a5c8602923e9dcee83dab9555d9aa
AnujAgrawal2004 <90625022+AnujAgrawal2004@users.noreply.github.com>
2023-04-11 09:22:00 +0530

    Time: 0 ms (100.00%), Space: 49.6 MB (60.60%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.java
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -1,11 +1,31 @@
 class Solution {
     public int hIndex(int[] citations) {
         int n = citations.length;
-        for(int i=0;i<n;i++){
-            if(n-i <= citations[i])
-                return n-i;
+        int start =0 ,end = n ,ans=0;
+        while(start<end){
+            int mid = start + (end-start)/2;
+            if(n-mid <= citations[mid]){
+                ans = Math.max(n-mid,ans);
+                end=mid;
+            }
+            else{
+                start=mid+1;
+            }
         }
-        return 0;
+        return ans;
+            
     }
     
 }
+
+
+
+// Brute-Force technique
+
+
+// int n = citations.length;
+//         for(int i=0;i<n;i++){
+//             if(n-i <= citations[i])
+//                 return n-i;
+//         }
+//         return 0;
