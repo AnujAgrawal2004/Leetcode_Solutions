eabd6670334b18dfb31beb8dce26d2fb65788b40
AnujAgrawal2004 <90625022+AnujAgrawal2004@users.noreply.github.com>
2023-04-13 08:33:58 +0530

    Time: 1 ms (100.00%), Space: 50.4 MB (84.04%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,18 +1,35 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int left[] = new int[nums.length];
-        left[0]=1;
-        for(int i=1;i<nums.length;i++){
-            left[i]=left[i-1]*nums[i-1];
-        }
-        int right[] = new int[nums.length];
-        right[nums.length-1] = 1;
-        for(int i=nums.length-2;i>=0;i--){
-            right[i] = right[i+1]*nums[i+1];
-        }
+       int temp=1;
+        int result[] = new int[nums.length];
         for(int i=0;i<nums.length;i++){
-            left[i]=left[i]*right[i];
+            result[i]=temp;
+            temp=temp*nums[i];
         }
-        return left;
+        temp = 1;
+        for(int i=nums.length-1;i>=0;i--){
+            result[i]=result[i]*temp;
+            temp=temp*nums[i];
+        }
+        return result;
     }
 }
+
+
+
+// Another approach using postfix and prefix sum
+
+// int left[] = new int[nums.length];
+//         left[0]=1;
+//         for(int i=1;i<nums.length;i++){
+//             left[i]=left[i-1]*nums[i-1];
+//         }
+//         int right[] = new int[nums.length];
+//         right[nums.length-1] = 1;
+//         for(int i=nums.length-2;i>=0;i--){
+//             right[i] = right[i+1]*nums[i+1];
+//         }
+//         for(int i=0;i<nums.length;i++){
+//             left[i]=left[i]*right[i];
+//         }
+//         return left;
