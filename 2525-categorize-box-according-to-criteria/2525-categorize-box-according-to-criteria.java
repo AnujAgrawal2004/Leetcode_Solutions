1928f19f759609b6bd7ed1eb1d5ab196bd8c9e93
AnujAgrawal2004 <90625022+AnujAgrawal2004@users.noreply.github.com>
2023-04-23 19:21:07 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (97.69%) - LeetHub

--- 2525-categorize-box-according-to-criteria/2525-categorize-box-according-to-criteria.java
+++ 2525-categorize-box-according-to-criteria/2525-categorize-box-according-to-criteria.java
@@ -1,45 +1,22 @@
-// class Solution {
-//     public String categorizeBox(int l, int w, int h, int m) {
-//         boolean isBulky = false;
-//         boolean isHeavy = false;
-//         long volume = l*w*h;
-//         if(l>=10000 || w>=10000 || h>=10000 || volume>= 1000000000)
-//             isBulky = true;
-//         if(m>=100)
-//             isHeavy = true;
-        
-//         if(isBulky && isHeavy)
-//             return "Both";
-//         if(isBulky == true && isHeavy == false)
-//             return "Bulky";
-//         if(isBulky == false && isHeavy == true)
-//             return "Heavy";
-//         return "Neither";
-//     }
-// }
-
-
-
 class Solution {
-    public String categorizeBox(int length, int width, int height, int mass) {
-        long vol = (long)length*width*height;
-        boolean b =false;
-        boolean h = false;
-        if(length>=10000 || width >= 10000 || height >=10000 || vol>=1000000000)
-        {
-            b=true;
-        }
-        if(mass>=100)
-        {
-            h=true;
-        }
-        if(b && h)
+    public String categorizeBox(int l, int w, int h, int m) {
+        boolean isBulky = false;
+        boolean isHeavy = false;
+        long volume = (long) l*w*h;
+        if(l>=10000 || w>=10000 || h>=10000 || volume>= 1000000000)
+            isBulky = true;
+        if(m>=100)
+            isHeavy = true;
+        
+        if(isBulky && isHeavy)
             return "Both";
-        else if (b==false && h==false)
-            return "Neither";
-        else if (b==true)
+       else if(isBulky == true && isHeavy == false)
             return "Bulky";
-        else
+       else if(isBulky == false && isHeavy == true)
             return "Heavy";
+        return "Neither";
     }
 }
+
+
+
